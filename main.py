2b22a7f5a7071968e808c3ac780b785be98b798e
Alextibtab <14alexcockburn@gmail.com>
2025-05-10 14:32:14 +0100

    changes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-class Book:
-    path = "books/"
+import sys
 
-    def __init__(self, title):
-        self.__title = title
+from stats import get_word_count
+
+class Book:
+    def __init__(self, path):
+        self.__title = path.split("/")[-1]
         self.__contents = []
-        with open(Book.path + title) as book:
+        with open(path) as book:
             self.__contents = book.read()
 
 
@@ -12,13 +14,13 @@ class Book:
         return self.__title
 
 
-    def get_word_count(self):
-        return len(self.__contents.split())
+    def get_contents(self):
+        return self.__contents
 
 
     def letter_frequency(self):
         letter_count = {}
-        for word in self.__contents:
+        for word in self.get_contents():
             for letter in word:
                 letter = letter.lower()
                 if letter in letter_count:
@@ -29,16 +31,17 @@ class Book:
 
 
     def book_report(self):
-        print(f"--- Begin report {Book.path+self.__title} ---")
-        print(f"{self.get_word_count()} words found in this document\n")
+        print(f"--- Begin report {self.get_title()} ---")
+        print(f"{get_word_count(self.get_contents())} words found in the document\n")
         freq = self.letter_frequency()
         freq_sorted = {key: val for key, val in sorted(freq.items(), key = lambda ele: ele[1], reverse = True) if key.isalpha()}
         for letter, count in freq_sorted.items():
-            print(f"The '{letter}' character was found {count} times")
+            print(f"{letter}: {count}")
         print("--- End report ---")
 
 if __name__ == "__main__":
-    frankenstein = Book("frankenstein.txt")
-    print(frankenstein.get_word_count())
-    print(frankenstein.letter_frequency())
-    frankenstein.book_report()
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = Book(sys.argv[1])
+    book.book_report()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_word_count(book_content):
+    return len(book_content.split())
